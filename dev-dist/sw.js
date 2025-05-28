d0fea11a88015f4cdbad3684e291df032a2e9a30
MalinowskiDesigns <malinowskidesignsss@gmail.com>
2025-05-28 19:04:07 +0200

    Variables normalized

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-3af34037'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.r2ug6j3u888"
+    "revision": "0.cckr1n99r98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
